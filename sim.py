88d3643eb47216429983fc91681963c3e7f242a3
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-10-21 12:46:47 -0500

    refactoring for triangulation

--- sim.py
+++ sim.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pyvisgraph as vg
-from matplotlib import pyplot as plt
 from os import listdir
+from scipy.spatial import Delaunay
+from matplotlib import pyplot as plt
 
 
 def main():
@@ -9,15 +10,14 @@ def main():
     files = listdir(folder)
     polygons = []
 
-    '''
     for f in files:
         x, y = np.loadtxt(folder + '/' + f)
         polygon = [vg.Point(x, y) for x, y in zip(x, y)]
         polygons.append(polygon)
 
+    '''
     start = vg.Point(13485.0, 57876.0)
     goal = vg.Point(12832.0, 42957.0)
-    '''
 
     polygons = [[vg.Point(0.0,1.0), vg.Point(3.0,1.0), vg.Point(1.5,4.0)],
                 [vg.Point(4.0,4.0), vg.Point(7.0,4.0), vg.Point(5.5,8.0)]]
@@ -37,11 +37,18 @@ def main():
         w = edge.p2
         plt.plot([v.x, w.x], [v.y, w.y], 'r')
 
-
+    '''
     for polygon in polygons:
         plot_polygon(polygon, 'b')
 
     plt.show()
+
+
+def triangulate(vertices):
+
+
+def plot_path(path):
+    pass
 
 
 def plot_polygon(polygon, color):
