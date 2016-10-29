9f581eca986060e841cd78901ea930eeefec7298
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-10-29 11:07:03 -0500

    formatted code

--- sim.py
+++ sim.py
@@ -6,8 +6,10 @@ from matplotlib import pyplot as plt
 
 def main():
     folder = 'obstacles'
+
     # load a list of the obstacle files.
     files = listdir(folder)
+
     # each element of the list of obstacles is a list
     # of vertices representing a figure on the plane.
     #
@@ -24,8 +26,10 @@ def main():
         x, y = np.loadtxt(folder + '/' + f)
         vertices = [vg.Point(x, y) for x, y in zip(x, y)]
         polygons.append(vertices)
-    # instantiate VisGraph ( https://github.com/TaipanRex/pyvisgraph )
-    g = vg.VisGraph()
+
+    # see https://github.com/TaipanRex/pyvisgraph
+    # for visibility graph source code
+    #
     # VisGraph builds the visibility graph using Der-Tsai Lee's
     # visibility graph algorithm. Lee's algorithm has
     # a total running time of O(n^2 log n), making it
@@ -40,7 +44,9 @@ def main():
     #     for each vertex v in G # O(n)
     #         do VG <- visible_vertices(v,S) # O(n log n)
     #     return VG
+    g = vg.VisGraph()
     g.build(polygons)
+
     # the VisGraph shortest_path function computes the shortest path
     # between two points using Dijkstra's algorithm. It adds
     # the start and goal nodes to the visibility graph along
@@ -49,6 +55,7 @@ def main():
     start = vg.Point(10000.0, 30000.0)
     goal = vg.Point(70597.0, 15000.0)
     shortest = g.shortest_path(start, goal)
+
     ################
     # PLOTING CODE #
     ################
@@ -74,9 +81,6 @@ def plot_polygon(vertices):
             w = vertices[0]
         else:
             w = vertices[i + 1]
-
-        plt.text(v.x, v.y, str(v.x) + ',' + str(v.y), fontsize=10)
-        plt.text(w.x, w.y, str(w.x) + ',' + str(w.y), fontsize=10)
         plt.plot([v.x, w.x], [v.y, w.y], 'r-')
 
 
