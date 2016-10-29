2a991b2d5d5492cfcb08cd197b7b4b9f105261e0
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-10-28 19:56:33 -0500

    finished

--- sim.py
+++ sim.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 def main():
     folder = 'obstacles'
-    # load a list of the obstacle file names
+    # load a list of the obstacle files.
     files = listdir(folder)
     # each element of the list of obstacles is a list
     # of vertices representing a figure on the plane.
@@ -24,19 +24,41 @@ def main():
         x, y = np.loadtxt(folder + '/' + f)
         vertices = [vg.Point(x, y) for x, y in zip(x, y)]
         polygons.append(vertices)
-
+    # instantiate VisGraph ( https://github.com/TaipanRex/pyvisgraph )
+    g = vg.VisGraph()
+    # VisGraph builds the visibility graph using Der-Tsai Lee's
+    # visibility graph algorithm. Lee's algorithm has
+    # a total running time of O(n^2 log n), making it
+    # much more efficient than the naive algorithm
+    # which checks every possible pair of vertices.
+    #
+    # LEE'S ALGORITHM PSEUDOCODE:
+    #
+    # visibility_graph(S <- disjoint polygonal obstacles)
+    #     G <- all vertices of S
+    #     VG -> empty visibility graph
+    #     for each vertex v in G # O(n)
+    #         do VG <- visible_vertices(v,S) # O(n log n)
+    #     return VG
+    g.build(polygons)
+    # the VisGraph shortest_path function computes the shortest path
+    # between two points using Dijkstra's algorithm. It adds
+    # the start and goal nodes to the visibility graph along
+    # with the corresponding edges.
+    # This fucntion runs in O(|E| + |V|log|V|)
     start = vg.Point(10000.0, 30000.0)
     goal = vg.Point(70597.0, 15000.0)
-
-    g = vg.VisGraph()
-    g.build(polygons)
     shortest = g.shortest_path(start, goal)
-    print(shortest)
+    ################
+    # PLOTING CODE #
+    ################
     for p in polygons:
         plot_polygon(p)
 
     for e in g.visgraph.get_edges():
-        plot_edge(e)
+       plot_edge(e)
+
+    plot_path(shortest)
 
     plt.plot(start.x, start.y, 'mo')
     plt.plot(goal.x, goal.y, 'mo')
@@ -62,6 +84,13 @@ def plot_edge(edge):
     v = edge.p1
     w = edge.p2
     plt.plot([v.x, w.x], [v.y, w.y], 'b--')
+
+
+def plot_path(path):
+    for i in range(len(path) - 1):
+        v = path[i]
+        w = path[i + 1]
+        plt.plot([v.x, w.x], [v.y, w.y], 'g-', linewidth=2.0)
 
 
 if __name__ == '__main__':
